0e5470c14961657ef3de0d95fc7c6e52ec417619
DESKTOP-DMG9FRR\molys <mosgang_work@hotmail.com>
2017-11-13 22:26:39 +0700

    update database

--- marathon.sql
+++ marathon.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2017 at 04:57 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Generation Time: Nov 13, 2017 at 04:25 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -41,8 +43,8 @@ CREATE TABLE `checkpointreport` (
 
 CREATE TABLE `emergencyreport` (
   `id` int(11) NOT NULL,
-  `header` varchar(255) NOT NULL,
-  `detail` varchar(255) NOT NULL,
+  `header` text NOT NULL,
+  `detail` text NOT NULL,
   `runnerId` int(11) NOT NULL,
   `staffId` int(11) NOT NULL,
   `lag` double NOT NULL,
@@ -57,7 +59,7 @@ CREATE TABLE `emergencyreport` (
 
 INSERT INTO `emergencyreport` (`id`, `header`, `detail`, `runnerId`, `staffId`, `lag`, `lng`, `status`, `level`) VALUES
 (1, 'test new emergency', 'test new emergency', 1, 3, 18.804320806128, 98.951153755188, 0, 1),
-(2, 'test', 'test', 1, 3, 18.80766723546, 98.953557014465, 0, 1);
+(2, '123123', '1123', 2, 3, 18.805268841449, 98.960251805547, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -80,8 +82,8 @@ CREATE TABLE `news` (
 
 CREATE TABLE `problemreport` (
   `id` int(11) NOT NULL,
-  `header` varchar(1024) NOT NULL,
-  `detail` varchar(1024) NOT NULL,
+  `header` text NOT NULL,
+  `detail` text NOT NULL,
   `senderId` int(11) NOT NULL,
   `staffId` int(11) NOT NULL,
   `lag` double NOT NULL,
@@ -143,21 +145,23 @@ CREATE TABLE `service` (
 
 CREATE TABLE `staff` (
   `id` int(11) NOT NULL,
-  `fname` varchar(1024) NOT NULL,
-  `lname` varchar(1024) NOT NULL,
-  `category` varchar(1024) NOT NULL,
-  `username` varchar(1024) NOT NULL,
-  `password` varchar(1024) NOT NULL
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `category` text NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `images` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staff`
 --
 
-INSERT INTO `staff` (`id`, `fname`, `lname`, `category`, `username`, `password`) VALUES
-(1, 'testFname', 'testLName', 'water', 'teststaff', 'passstaff'),
-(2, 'firstname', 'lastname', 'checkpoint', 'username1', 'password1'),
-(3, 'test', 'test', 'water', 'asd', '123qwe');
+INSERT INTO `staff` (`id`, `fname`, `lname`, `category`, `username`, `password`, `images`) VALUES
+(1, 'testFname', 'testLName', 'water', 'teststaff', 'passstaff', ''),
+(2, 'firstname', 'lastname', 'checkpoint', 'username1', 'password1', ''),
+(7, 'qweqwe', 'qweqwe', 'qweqw', 'qweqwe', 'qweq', '20170810_145804.jpg'),
+(8, 'test', 'test', 'test', 'test', 'test', '20170810_145753.jpg');
 
 --
 -- Indexes for dumped tables
@@ -214,36 +218,44 @@ ALTER TABLE `staff`
 --
 ALTER TABLE `checkpointreport`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `emergencyreport`
 --
 ALTER TABLE `emergencyreport`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `problemreport`
 --
 ALTER TABLE `problemreport`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `runner`
 --
 ALTER TABLE `runner`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
